2a4983a953642cdc2c4d218b0174457abe0a0a85
Baris Balli <barisballi@hotmail.com>
2020-03-02 20:54:23 +0000

    Data Refresh 02/03/2020

--- combinedStats.R
+++ combinedStats.R
@@ -3,6 +3,8 @@ library(readr)
 library(tidyr)
 library(lubridate)
 
+
+
 pldata1920 <- read_csv("Premier League Table 1920.csv")
 pldata1819 <- read_csv("Premier League Table 1819 2.csv")
 pldata1718 <- read_csv("Premier League Table 1718.csv")
